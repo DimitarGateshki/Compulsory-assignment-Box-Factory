f584c20a1ba3e870ae1138aac1c088e5308e1980
DimitarGateshki <101872373+DimitarGateshki@users.noreply.github.com>
2023-10-03 06:24:30 +0200

    Set pop up
    Set pop up components displaying

--- Fromt-end/my-app/src/app/app.module.ts
+++ Fromt-end/my-app/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CreatePopUpComponent } from './components/create-pop-up/create-pop-up.component';
-import { EditPipUpComponent } from './components/edit-pip-up/edit-pip-up.component';
 import { DelPipUpComponent } from './components/del-pip-up/del-pip-up.component';
 import { BoxComponentComponent } from './components/box-component/box-component.component';
+import { EditPipUpComponent } from './components/edit-pip-up/edit-pip-up.component';
 
 @NgModule({
   declarations: [
