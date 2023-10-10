eb1a38d2a81c4e575cca3fcdf8b8d580cf930adc
AtanasLS <atanaslstoyanov.03@gmail.com>
2023-10-10 15:00:33 +0200

    adding some info

--- Back-End/FullBackendTestProject/api/Program.cs
+++ Back-End/FullBackendTestProject/api/Program.cs
@@ -10,9 +10,14 @@ builder.Services.AddSingleton<Service>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSpaStaticFiles(conf => conf.RootPath = "../path to www directory");
+
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.MapControllers();
 app.UseMiddleware<GlobalExceptionHandler>();
+app.UseSpaStaticFiles();
+app.UseSpa(conf => conf.Options.SourcePath = "../path to www directory");
 app.Run();
