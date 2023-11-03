7cbf92d1764a144f00c11f2e6aa680f085f1afc3
AtanasLS <atanaslstoyanov.03@gmail.com>
2023-11-03 02:20:15 +0100

    adding more logic into ContextConfig and into the DeleteById Test.

--- Back-End/FullBackendTestProject/Test/ContextConfig.cs
+++ Back-End/FullBackendTestProject/Test/ContextConfig.cs
@@ -1,12 +1,74 @@
 ﻿using Npgsql;
+using System;
+using Dapper;
+
 
 namespace Test;
 
 
-    public class ContextConfig
+   public class ContextConfig
     {
-        public static readonly NpgsqlDataSource DataSource; 
+        public static readonly NpgsqlDataSource DataSource;
         public static readonly string ClientAppBaseUrl = "http://localhost:5000";
         public static readonly string ApiBaseUrl = "http://localhost:5000/api";
 
+        static ContextConfig()
+        {
+            var envVarKeyName = "pgconn";
+
+            var rawConnectionString = Environment.GetEnvironmentVariable(envVarKeyName);
+            if (string.IsNullOrEmpty(rawConnectionString))
+            {
+                throw new Exception("Environment variable 'pgconn' is not set.");
+            }
+
+            try
+            {
+                var uri = new Uri(rawConnectionString);
+                var properlyFormattedConnectionString = new NpgsqlConnectionStringBuilder
+                {
+                    Host = uri.Host,
+                    Database = uri.AbsolutePath.Trim('/'),
+                    Username = uri.UserInfo.Split(':')[0],
+                    Password = uri.UserInfo.Split(':')[1],
+                    Port = uri.Port > 0 ? uri.Port : 5432,
+                    Pooling = false
+                }.ToString();
+
+                DataSource = new NpgsqlDataSourceBuilder(properlyFormattedConnectionString).Build();
+                DataSource.OpenConnection().Close();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("There is a problem with the pgconn connection string.", e);
+            }
+        }
+
+        public static void TriggerRebuild()
+        {
+            using (var conn = DataSource.OpenConnection())
+            {
+                try
+                {
+                    conn.Execute(RebuildScript);
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("There was a problem rebuilding the database. Please check your PostgreSQL setup.", e);
+                }
+            }
+        }
+
+        public static string RebuildScript = @"
+DROP SCHEMA IF EXISTS test_schema CASCADE;
+CREATE SCHEMA test_schema;
+create table if not exists test_schema.boxes
+(
+    id serial PRIMARY KEY,
+    name text,
+    date_of_creation date,
+    category text
+);
+";
     }
+
--- Back-End/FullBackendTestProject/Test/Tests.cs
+++ Back-End/FullBackendTestProject/Test/Tests.cs
@@ -3,15 +3,18 @@ using System.Text;
 using Microsoft.Playwright.NUnit;
 using Newtonsoft.Json;
 using Test;
+using Dapper;
+
+
 
 
 
 namespace Test;
 
 
-public class Tests 
+public class Tests
 {
-    
+
     [Test]
     public async Task TestGetBoxes()
     {
@@ -19,7 +22,7 @@ public class Tests
         {
             HttpResponseMessage response = await client.GetAsync($"{ContextConfig.ClientAppBaseUrl}/boxes");
 
-            
+
             if (response.IsSuccessStatusCode)
             {
                 string responseContent = await response.Content.ReadAsStringAsync();
@@ -31,6 +34,7 @@ public class Tests
             }
         }
     }
+
     [Test]
     public async Task TestGetBoxById()
     {
@@ -48,65 +52,88 @@ public class Tests
             {
                 Assert.Fail($"API request failed with status code: {response.StatusCode}");
             }
+
+        }
+
+    }
+
+    [Test]
+
+    public async Task TestPostBox()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            var boxData = new Box()
+            {
+                BoxName = "TestBoxTest",
+                DateOfCreation = DateTime.Now,
+                BoxCategory = "sold"
+            };
+
+            HttpResponseMessage response = await client.PostAsJsonAsync($"{ContextConfig.ApiBaseUrl}/NewBox", boxData);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("POST request was successful.");
+            }
+            else
+            {
+                Console.WriteLine($"POST request failed with status code: {response.StatusCode}");
+                string responseContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Response content: {responseContent}");
+            }
+
+            Assert.IsTrue(response.IsSuccessStatusCode, "POST request should be successful.");
+        }
+    }
+
+    [Test]
+    public async Task TestDeleteBox()
+    {
+        // rebuild and insert a row with the ID to delete
+        ContextConfig.TriggerRebuild();
+        
+        using (var conn = await ContextConfig.DataSource.OpenConnectionAsync())
+        {
+
+            var box = new Box
+            {
+                BoxId=1,
+                BoxName = "YourName",
+                DateOfCreation = DateTime.Now,
+                BoxCategory = "YourCategory"
+            };
+
+            var insertedBox = conn.QueryFirst<Box>(
+                "INSERT INTO test_schema.boxes (name, date_of_creation, category) VALUES (@BoxName, @DateOfCreation, @BoxCategory) RETURNING *;",
+                box);
+
             
+            int boxIdToDelete = 1;
+            
+            using (HttpClient client = new HttpClient())
+            {
+
+                HttpResponseMessage response =
+                    await client.DeleteAsync($"{ContextConfig.ApiBaseUrl}/DeleteBox/{boxIdToDelete}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"DELETE request for Box ID {boxIdToDelete} was successful.");
+                }
+                else
+                {
+                    Console.WriteLine($"DELETE request failed with status code: {response.StatusCode}");
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Response content: {responseContent}");
+                }
+
+                Assert.AreEqual(200, (int)response.StatusCode,
+                    "DELETE request should return 204 No Content on success.");
+            }
         }
-    
+
     }
-
-[Test]
-
- public async Task TestPostBox()
-	{
-    using (HttpClient client = new HttpClient())
-    {
-        var boxData = new Box()
-        {
-            BoxName = "TestBoxTest",
-            DateOfCreation = DateTime.Now,
-            BoxCategory = "sold"
-        };
-
-        HttpResponseMessage response = await client.PostAsJsonAsync($"{ContextConfig.ApiBaseUrl}/NewBox", boxData);
-
-        if (response.IsSuccessStatusCode)
-        {
-            Console.WriteLine("POST request was successful.");
-        }
-        else
-        {
-            Console.WriteLine($"POST request failed with status code: {response.StatusCode}");
-            string responseContent = await response.Content.ReadAsStringAsync();
-            Console.WriteLine($"Response content: {responseContent}");
-        }
-
-        Assert.IsTrue(response.IsSuccessStatusCode, "POST request should be successful.");
-    }
-}
-
-[Test]
-public async Task TestDeleteBox()
-{
-    using (HttpClient client = new HttpClient())
-    {
-        int boxIdToDelete = ; 
-
-        HttpResponseMessage response = await client.DeleteAsync($"{ContextConfig.ApiBaseUrl}/DeleteBox/{boxIdToDelete}");
-
-        if (response.IsSuccessStatusCode)
-        {
-            Console.WriteLine($"DELETE request for Box ID {boxIdToDelete} was successful.");
-        }
-        else
-        {
-            Console.WriteLine($"DELETE request failed with status code: {response.StatusCode}");
-            string responseContent = await response.Content.ReadAsStringAsync();
-            Console.WriteLine($"Response content: {responseContent}");
-        }
-
-        Assert.AreEqual(200, (int)response.StatusCode, "DELETE request should return 204 No Content on success.");
-    }
-}
-
 }
 
     
